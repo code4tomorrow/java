5f5793c0bf917eabb736b9d943e832cce3e73afb
Alveera Munshi <sporty.alveera@gmail.com>
2020-05-31 19:03:24 -0400

    Created practice folder and templates

---
+++ src/com/codefortomorrow/beginner/chapter9/practice/ParallelArrayGradedAssignmentAMunshi.java
@@ -0,0 +1,14 @@
+package parallelArrayGradedAssignmentAMunshi;
+
+import java.util.Scanner;
+
+public class ParallelArrayGradedAssignmentAMunshi 
+{
+
+	public static void main(String[] args) 
+	{
+		/*In this assignment, you will be required to have 5 different categories of information regarding a single topic that include String, int, and double array types. The arrays for the program should have 5 elements each. The program should prestore the information. However, the program should allow a Scanner to access information of user choice. You may store the information to the arrays however you see fit, but once it is stored a user should be able to ask for a specific stat for your set of data.  The program should loop until the user enters “quit.  Once the user types quit, the program should end. You are able to use any notes or previous programs to help aid you in creating this assignment. */
+
+	}
+
+}
