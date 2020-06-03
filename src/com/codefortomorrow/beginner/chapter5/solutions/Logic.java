8f57d90f5f644608211211d57cc76acc567e5a6f
Rebecca Dang <ph.rdang@gmail.com>
2020-06-03 11:47:58 -0700

    Added Ch. 5 logic practice

---
+++ src/com/codefortomorrow/beginner/chapter5/solutions/Logic.java
@@ -0,0 +1,23 @@
+package com.codefortomorrow.beginner.chapter5.solutions;
+
+/*
+ * Given the code below, predict what the output
+ * of this program will be. Write your answers
+ * as comments on the program next to the print
+ * statements.
+ */
+
+public class Logic {
+	public static void main(String[] args) {
+		int z = 5;
+		int y = 25;
+
+		System.out.println(z < 10 && y == z * 5); // true
+		System.out.println(z < 10 && y != z * 5); // false
+		System.out.println(z < 10 || y == z * 5); // true
+		System.out.println(z < 10 || y != z * 5); // true
+
+		System.out.println(y == z * 5); // true
+		System.out.println(y != z * 5); // false
+	}
+}
