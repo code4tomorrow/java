59ef296f043b237700d2cae0b4f6152a45dafef1
JJ27 <66833672+JJ27@users.noreply.github.com>
2020-06-25 17:51:54 -0400

    Create StringSlicer Problem
    Some practice with String Methods

---
+++ src/com/codefortomorrow/beginner/chapter3/practice/StringSlicer.java
@@ -0,0 +1,11 @@
+public class StringSlicer{
+  /*
+  Use String Methods to convert the following strings to the results
+  a) "A Tale of Two Cities" -> "WO CIT"
+  b) "A Handmaid's Tale" -> "s"
+  c) "wE LoVe tO CoDe" -> "we love to cod"
+  */
+  public static void main(String[] args){
+
+  }
+}
