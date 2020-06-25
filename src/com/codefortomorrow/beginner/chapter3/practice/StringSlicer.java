98261ac6cebab6d7816b97b5a3b2ec3385f39b0e
JJ27 <66833672+JJ27@users.noreply.github.com>
2020-06-25 18:37:45 -0400

    Add package and move instructions
    Added package
    Moved instructions to correct location

--- src/com/codefortomorrow/beginner/chapter3/practice/StringSlicer.java
+++ src/com/codefortomorrow/beginner/chapter3/practice/StringSlicer.java
@@ -1,11 +1,14 @@
-public class StringSlicer{
-  /*
-  Use String Methods to convert the following strings to the results
-  a) "A Tale of Two Cities" -> "WO CIT"
-  b) "A Handmaid's Tale" -> "s"
-  c) "wE LoVe tO CoDe" -> "we love to cod"
-  */
-  public static void main(String[] args){
+package com.codefortomorrow.beginner.chapter3.practice;
 
+/*
+ * Use String Methods to convert the following strings to the results
+ * a) "A Tale of Two Cities" -> "WO CIT"
+ * b) "A Handmaid's Tale" -> "s"
+ * c) "wE LoVe tO CoDe" -> "e love to code"
+ */
+
+public class StringSlicer{
+  public static void main(String[] args){
+    //Write Code here
   }
 }
--- src/com/codefortomorrow/beginner/chapter3/solutions/StringSlicer.java
+++ src/com/codefortomorrow/beginner/chapter3/solutions/StringSlicer.java
@@ -1,10 +1,12 @@
+package com.codefortomorrow.beginner.chapter3.solutions;
+
+/*
+ * Use String Methods to convert the following strings to the results
+ * a) "A Tale of Two Cities" -> "WO CIT"
+ * b) "A Handmaid's Tale" -> "s"
+ * c) "wE LoVe tO CoDe" -> "e love to code"
+ */
 public class StringSlicer{
-  /*
-  Use String Methods to convert the following strings to the results
-  a) "A Tale of Two Cities" -> "WO CIT"
-  b) "A Handmaid's Tale" -> "s"
-  c) "wE LoVe tO CoDe" -> "e love to code"
-  */
   public static void main(String[] args){
     //Initialize original strings
     String s1 = "A Tale of Two Cities";
