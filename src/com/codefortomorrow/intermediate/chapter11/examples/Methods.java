b87afac41f9b0f0fd44b2a351804f0c5375d9ab1
Rebecca Dang <ph.rdang@gmail.com>
2020-07-05 17:40:33 -0700

    Add Methods example code
    request-checks:true

---
+++ src/com/codefortomorrow/intermediate/chapter11/examples/Methods.java
@@ -0,0 +1,13 @@
+package com.codefortomorrow.intermediate.chapter11.examples;
+
+public class Methods {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            printMessage();
+        }
+    }
+
+    public static void printMessage() {
+        System.out.println("Hello World");
+    }
+}
