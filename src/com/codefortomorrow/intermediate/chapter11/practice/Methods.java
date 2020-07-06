61ebacd4d94bd5ca8850e6e7584bdac1f071ae87
Achintya Rajan <39139575+ARajan1084@users.noreply.github.com>
2020-07-05 17:15:05 -0700

    Added a practice problem

--- src/com/codefortomorrow/intermediate/chapter11/practice/Methods.java
+++ src/com/codefortomorrow/intermediate/chapter11/practice/Methods.java
@@ -1,4 +1,14 @@
 package com.codefortomorrow.intermediate.chapter11.practice;
 
 public class Methods {
+
+    /**
+     * Difficulty: 1
+     *
+     * Print the first n even integers
+     * @param n number of even integers to print
+     */
+    public static void printEvens(int n) {
+
+    }
 }
--- src/com/codefortomorrow/intermediate/chapter11/solutions/Methods.java
+++ src/com/codefortomorrow/intermediate/chapter11/solutions/Methods.java
@@ -1,4 +1,22 @@
 package com.codefortomorrow.intermediate.chapter11.solutions;
 
 public class Methods {
+
+    /**
+     * Difficulty: 2
+     *
+     * Print the first n even integers
+     * @param n number of even integers to print
+     */
+    public static void printEvens(int n) {
+        int evens = 0;
+        int i = 0;
+        while (evens < n) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+                n++;
+            }
+            i++;
+        }
+    }
 }
