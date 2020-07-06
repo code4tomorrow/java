653187615bfae3af58c217ee4e9c4df3a3c6bd0e
Achintya Rajan <39139575+ARajan1084@users.noreply.github.com>
2020-07-05 17:20:41 -0700

    added a lower difficulty problem

--- src/com/codefortomorrow/intermediate/chapter11/practice/Methods.java
+++ src/com/codefortomorrow/intermediate/chapter11/practice/Methods.java
@@ -5,6 +5,18 @@ public class Methods {
     /**
      * Difficulty: 1
      *
+     * Returns the average of two doubles
+     * @param a the first double
+     * @param b the second double
+     * @return average
+     */
+    public static double average(double a, double b) {
+        return 0.0; //TODO: Fix!
+    }
+
+    /**
+     * Difficulty: 1
+     *
      * Print the first n even integers
      * @param n number of even integers to print
      */
--- src/com/codefortomorrow/intermediate/chapter11/solutions/Methods.java
+++ src/com/codefortomorrow/intermediate/chapter11/solutions/Methods.java
@@ -3,6 +3,18 @@ package com.codefortomorrow.intermediate.chapter11.solutions;
 public class Methods {
 
     /**
+     * Difficulty: 1
+     * 
+     * Returns the average of two doubles
+     * @param a the first double
+     * @param b the second double
+     * @return average
+     */
+    public static double average(double a, double b) {
+        return (a + b)/2;
+    }
+
+    /**
      * Difficulty: 2
      *
      * Print the first n even integers
