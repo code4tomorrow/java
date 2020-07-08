8365dc736cb0e843811511711fd118ecaafe04a7
Rebecca Dang <ph.rdang@gmail.com>
2020-07-07 23:08:57 -0700

    Testing workflow

---
+++ src/com/codefortomorrow/intermediate/chapter10/examples/TestWorkflow.java
@@ -0,0 +1,14 @@
+package com.codefortomorrow.intermediate.chapter10.examples;
+
+public class TestWorkflow {
+    public static void main(String[] args){
+        //testing the code inspector 
+            //github action
+        System.out.println (
+            "hello");
+        /*
+        stsigksdhgkdshgksdhgkdhgkhgskghfkdghfkdghdfkghfdkghfdkjhgfkdjhgkdjfhgkdfj
+        */
+    }
+    
+}
