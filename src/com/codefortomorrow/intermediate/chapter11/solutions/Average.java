911e9bb8c31ff3c05e57fb1f12dafa0d2c44e5a2
Rebecca Dang <ph.rdang@gmail.com>
2020-07-10 09:16:35 -0700

    Add instructions, comments to Average and Print Evens
    Clarified instructions, added main method for students
    to test their methods. Also deleted method blocks
    so that students can practice declaring methods.
    Also deleted JavaDoc comments to let students
    practice writing JavaDoc comments.

--- src/com/codefortomorrow/intermediate/chapter11/solutions/Average.java
+++ src/com/codefortomorrow/intermediate/chapter11/solutions/Average.java
@@ -1,14 +1,33 @@
 package com.codefortomorrow.intermediate.chapter11.solutions;
 
+/*
+Code a method called average that takes
+two double parameters, a and b.
+The method should return the average
+of the two doubles.
+
+Test if your average method works in the
+main method by calling it at least 3 times
+with different arguments and printing the results.
+
+Bonus points if you write a JavaDoc comment for
+the average() method.
+*/
+
 public class Average {
 
+    public static void main(String[] args) {
+        // test the average method
+        System.out.println(average(5, 4)); // prints 4.5
+        System.out.println(average(0, 10)); // prints 5.0
+        System.out.println(average(1.5, 3.75)); // prints 2.625
+    }
+
     /**
-     * Difficulty: 1
-     *
      * Returns the average of two doubles
      * @param a the first double
      * @param b the second double
-     * @return average
+     * @return the average of two doubles
      */
     public static double average(double a, double b) {
         return (a + b) / 2;
