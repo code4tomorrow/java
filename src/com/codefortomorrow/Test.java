c78099a1d5af7b65f1c6674410202f9769a513cf
Rebecca Dang <ph.rdang@gmail.com>
2020-08-29 11:01:37 -0700

    Try master instead of v0.3.1, add test class

---
+++ src/com/codefortomorrow/Test.java
@@ -0,0 +1,13 @@
+package com.codefortomorrow;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+
+    public static void test() {
+        System.out.println("Testing");
+        System.out.println("Testing");
+        System.out.println("Testing");
+    }
+}
