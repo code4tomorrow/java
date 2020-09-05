5b614d85e4c5fa6edd45661008a02149bea1f541
Rebecca Dang <ph.rdang@gmail.com>
2020-09-05 11:06:58 -0700

    Clarify code, add braces, remove Math import

--- src/com/codefortomorrow/advanced/chapter13/solutions/PrimePractice.java
+++ src/com/codefortomorrow/advanced/chapter13/solutions/PrimePractice.java
@@ -1,53 +1,68 @@
-package com.codefortomorrow.advanced.chapter13.practice;
+package com.codefortomorrow.advanced.chapter13.solutions;
+
+import java.util.Scanner;
 
 /*
 Write a method called isPrime which returns
 true if the given integer is prime and false otherwise.
 
-This is similar to the chapter 11 problem, but this time write
+This is similar to the Chapter 11 problem, but this time write
 your method using recursion.
 
-In your main method, include a scanner so the user can check
+In your main method, include a Scanner so the user can check
 as many numbers as they want until they enter -1.
 
 Note: There are more complex solutions, but this is the fastest one
 within the scope of this chapter.
 */
 
-import java.lang.Math;
-import java.util.Scanner;
-
 public class PrimePractice {
 
     public static void main(String[] args) {
-        int s = 0;
-        while (s != -1) {
-            Scanner reader = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        int number = 0;
+        while (number != -1) {
             System.out.print("Enter an integer to check: ");
-            s = reader.nextInt();
-            if (s != -1) {
-                if (isPrime(s, 2)) System.out.println(
-                    "That is a prime!"
-                ); else System.out.println("Not a prime!");
+            number = reader.nextInt();
+            if (number != -1) {
+                if (isPrime(number, 2)) {
+                    System.out.println("That is a prime!");
+                } else { 
+                    System.out.println("Not a prime!");
+                }
             }
         }
+        reader.close();
     }
 
     /**
      * isPrime checks the primality of a given integer
-     * @param n The integer to check
-     * @param z Current Divisor(used for recursion)
+     * @param n  The integer to check
+     * @param z  Current divisor (used for recursion)
      * @return boolean true if prime and false if not
      */
     public static boolean isPrime(int n, int z) {
-        //Check base cases
-        if (n <= 2) return (n == 2) ? true : false;
-        //Ternary operator used there
-        if (n % z == 0) return false;
-        //If z gets high enough that z > sqrt(n), then n is prime, because factors just repeat after
-        if (Math.pow(z, 2) > n) return true;
+        // Check base cases
+        if (n <= 2) {
+            return n == 2;
+        }
+        
+        // If n is divisible by the current divisor,
+        // it has a factor other than 1 and thus is
+        // not prime
+        if (n % z == 0) {
+            return false;
+        }
 
-        //If none of the above work
+        // If z gets high enough that z > sqrt(n), then n is prime,
+        // because factors just repeat after
+        if (z > Math.sqrt(n)) {
+            return true;
+        }
+
+        // If none of the above work,
+        // keep calling isPrime recursively
+        // with a larger divisor
         return isPrime(n, z + 1);
     }
 }
