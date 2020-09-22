9e30ab7e422401eab48d2e17b3fa3a4a5494fdbd
neha-peddinti <56367930+neha-peddinti@users.noreply.github.com>
2020-09-21 22:41:41 -0400

    Added examples and adv. subdirectories

---
+++ src/com/codefortomorrow/advanced/chapter14/examples/FileIO.java
@@ -0,0 +1,28 @@
+package com.codefortomorrow.advanced.chapter14.examples
+import java.io.*;
+
+public class FileIO {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        File file = new File("names.txt");
+        
+        try {
+            in = new FileInputStream(file);
+            out = new FileOutputStream("names_upper.txt");
+            int c;
+            while ((c = in.read()) != -1)
+                out.write(Character.toUpperCase((char)c));
+
+        } catch (IOException e) {
+            System.out.println("An IO Exception occurred.");
+            e.printStackTrace();  // Prints error output stream.
+        } finally {
+            if (in != null)
+                in.close();
+            if (out != null)
+                out.close();
+        }
+    }
+}
---
+++ src/com/codefortomorrow/advanced/chapter14/examples/Throws.java
@@ -0,0 +1,19 @@
+package com.codefortomorrow.advanced.chapter14.examples;
+import java.io.*;
+
+public class Throws {
+
+    public static void main(String[] args) {
+        try {
+            File file = null;
+            access(file);
+        } catch(IOException e) {
+            System.out.println("There was an IOException.");
+        }
+    }
+
+    public static void access(File file) throws IOException {
+        if(file == null)
+            throw new IOException();
+    }
+}
