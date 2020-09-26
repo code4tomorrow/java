685ad6d7892045c801c49b51e58c3384798c9151
ARajan1084 <ARajan1084@users.noreply.github.com>
2020-09-26 23:42:53 +0000

    Bot: Prettified Java code!

--- src/com/codefortomorrow/advanced/chapter13/examples/Fibonacci.java
+++ src/com/codefortomorrow/advanced/chapter13/examples/Fibonacci.java
@@ -1,6 +1,7 @@
 package com.codefortomorrow.advanced.chapter13.examples;
 
 public class Fibonacci {
+
     public static int fibonacciRecursion(int nthNumber) {
         //use recursion
         if (nthNumber == 0) {
@@ -8,19 +9,21 @@ public class Fibonacci {
         } else if (nthNumber == 1) {
             return 1;
         }
-        return fibonacciRecursion(nthNumber - 1) + fibonacciRecursion(nthNumber - 2);
+        return (
+            fibonacciRecursion(nthNumber - 1) +
+            fibonacciRecursion(nthNumber - 2)
+        );
     }
 
     public static int fibonacciLoop(int nthNumber) {
         //use loop
         int previouspreviousNumber, previousNumber = 0, currentNumber = 1;
-        for (int i = 1; i < nthNumber ; i++) {
+        for (int i = 1; i < nthNumber; i++) {
             previouspreviousNumber = previousNumber;
             previousNumber = currentNumber;
             currentNumber = previouspreviousNumber + previousNumber;
         }
         return currentNumber;
     }
-
     // Credit: https://dev.to/khalilsaboor/fibonacci-recursion-vs-iteration--474l
 }
