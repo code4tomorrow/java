1df26ee4950cff0dbf24ab35fd0451d45ddc34fa
JoshJ <hellojosh04@gmail.com>
2020-09-27 16:28:11 -0400

    Measurable Problem

---
+++ src/com/codefortomorrow/advanced/chapter15/practice/MeasurableProblem.java
@@ -0,0 +1,24 @@
+package com.codefortomorrow.advanced.chapter15.practice;
+/*
+Create a Measurable interface
+that is used to “measure” anything. Add necessary methods.
+Required Methods: double average(Measurable[] items),
+largest(Measurable[] items) or smallest(Measurable[] items).
+Create a Student class that implements Measurable and
+it’s methods(you can compare whatever you measure, i.e. GPA).
+Note: for the largest/smallest methods don’t worry
+about the case if they are equal.
+
+Problem adapted from Java SE 9 Textbook
+*/
+public class MeasurableProblem{
+  public static void main(String[] args){
+    //Add testing code
+  }
+}
+interface Measurable{
+  //add interface code here
+}
+class Student implements Measurable{
+  //Add student code here
+}
