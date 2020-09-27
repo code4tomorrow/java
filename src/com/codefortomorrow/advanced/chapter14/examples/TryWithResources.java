d7220b0cd04012f195ca8900be34328949c47fcf
JJ27 <JJ27@users.noreply.github.com>
2020-09-27 16:10:27 +0000

    Bot: Prettified Java code!

--- src/com/codefortomorrow/advanced/chapter14/examples/TryWithResources.java
+++ src/com/codefortomorrow/advanced/chapter14/examples/TryWithResources.java
@@ -1,4 +1,5 @@
 package com.codefortomorrow.advanced.chapter14.examples;
+
 import java.io.*;
 
 public class TryWithResources {
@@ -6,16 +7,17 @@ public class TryWithResources {
     public static void main(String[] args) throws IOException {
         File file = new File("names.txt");
 
-        try (FileInputStream in = new FileInputStream(file);
-            FileOutputStream out = new FileOutputStream("names_upper.txt")) {
-
+        try (
+            FileInputStream in = new FileInputStream(file);
+            FileOutputStream out = new FileOutputStream("names_upper.txt")
+        ) {
             int c;
-            while ((c = in.read()) != -1)
-                out.write(Character.toUpperCase((char)c));
-
+            while ((c = in.read()) != -1) out.write(
+                Character.toUpperCase((char) c)
+            );
         } catch (IOException e) {
             System.out.println("An IO Exception occurred.");
-            e.printStackTrace();  // Prints error output stream.
+            e.printStackTrace(); // Prints error output stream.
         }
     }
 }
