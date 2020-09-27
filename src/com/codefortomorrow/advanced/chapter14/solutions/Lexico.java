4b404bdde19ed21ee0cb9fabfdcb24ce3b3c5bee
JJ27 <JJ27@users.noreply.github.com>
2020-09-27 20:36:02 +0000

    Bot: Prettified Java code!

--- src/com/codefortomorrow/advanced/chapter14/solutions/Lexico.java
+++ src/com/codefortomorrow/advanced/chapter14/solutions/Lexico.java
@@ -16,22 +16,23 @@ Hint: Java’s String.compareTo method may be of use to you.
 public class Lexico {
 
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new FileReader("f1.txt"));
-        BufferedReader br2 = new BufferedReader(new FileReader("f2.txt"));) {
+        try (
+            BufferedReader br = new BufferedReader(new FileReader("f1.txt"));
+            BufferedReader br2 = new BufferedReader(new FileReader("f2.txt"));
+        ) {
             String line1 = br.readLine();
             String line2 = br2.readLine();
             int c = line1.compareTo(line2);
-            while (c == 0){
-              line1 = br.readLine();
-              line2 = br2.readLine();
-              c = line1.compareTo(line2);
+            while (c == 0) {
+                line1 = br.readLine();
+                line2 = br2.readLine();
+                c = line1.compareTo(line2);
             }
-            if(c > 0)
-              System.out.println("File 2");
-            else
-              System.out.println("File 1");
-        } catch(IOException e){
-          e.printStackTrace();
+            if (c > 0) System.out.println("File 2"); else System.out.println(
+                "File 1"
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
