539eded473f5fbbc4a6379d3ab59085b8aa999e2
DelosReyesJordan <63662211+DelosReyesJordan@users.noreply.github.com>
2020-09-27 23:55:32 -0700

    Create Restaurants.java
    Add an easy abstract class/abstract method problem

---
+++ src/com/codefortomorrow/advanced/chapter15/practice/Restaurants.java
@@ -0,0 +1,16 @@
+abstract class Restaurant {
+
+    //insert abstract method
+
+}
+
+public class AbstractClassesTutorial {
+
+  //Create 4 classes that extend the abstract class
+
+    public static void main(String[] args) {
+        
+        //print out the 4 abstract classes
+
+    }
+}
