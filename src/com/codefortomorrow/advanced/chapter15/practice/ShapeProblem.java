374b514cd44abff4385e291e5f066fb1ae1d2190
Richard Cao <44596884+Richard-Cao226@users.noreply.github.com>
2020-09-28 09:24:25 -0700

    Create ShapeProblem.java

---
+++ src/com/codefortomorrow/advanced/chapter15/practice/ShapeProblem.java
@@ -0,0 +1,23 @@
+package com.codefortomorrow.advanced.chapter15.practice;
+
+/*
+Create class Square and class Triangle
+that implement the interface Shape.
+In the sides method, explain how many sides each shape has.
+Ex.  “I have ___ sides!”
+ */
+
+
+public class ShapeProblem {
+    public static void main(String[] args) {
+        // add test code here
+    }
+}
+
+interface Shape {
+    public abstract void sides();
+}
+
+// create square class here
+
+// create triangle class here
