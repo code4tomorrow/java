b9f1238a4ddced74abe2b69d56b56ec4f9a84dca
ArmeetJatyani <armeetjatyani@gmail.com>
2021-03-15 11:53:40 -0700

    ApplesOranges exercise complete

---
+++ src/com/codefortomorrow/beginner/chapter2/solutions/ApplesOranges.java
@@ -0,0 +1,28 @@
+package com.codefortomorrow.beginner.chapter2.practice;
+
+/**
+ * @author ArmeetJatyani
+ * March 2021
+ * 
+ * Print out the number of apples and oranges!
+ */
+
+ public class ApplesOranges {
+    public static void main(String[] args) {
+        // write your code here
+        
+        // define an integer variable called numOranges with value 10 (line 15)
+        int numOranges = 10;
+        
+        // define an integer variable called numApples with value 24 (line 18)
+        int numApples = 24;
+        
+        // print out number of oranges, output: "I have 10 oranges." (line 21)
+        System.out.println("I have " + numOranges + " oranges.");
+
+        // print out number of apples, output: "I have 24 apples." (line 24)
+        System.out.println("I have " + numApples + " apples.");
+
+    }
+ }
+   
