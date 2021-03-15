bb68ef08cfdb2d99dba88794b254f703e2a55b8f
ArmeetJatyani <armeetjatyani@gmail.com>
2021-03-15 11:47:45 -0700

    finished VariableTypes exercise and solution

---
+++ src/com/codefortomorrow/beginner/chapter2/solutions/VariableTypes.java
@@ -0,0 +1,34 @@
+package com.codefortomorrow.beginner.chapter1.practice;
+
+/**
+ * @author ArmeetJatyani
+ * March 2021
+ * 
+ * Define different types of variables
+ */
+
+ public class VariableTypes {
+    public static void main(String[] args) {
+        // write your code here
+        
+        // define an integer variable on line 15
+        int age = 23;
+        
+        // define a float variable on line 18
+        float pi = 3.14f;
+        
+        // define a double variable on line 21
+        double decimal = 23.2352536d;
+
+        // define a boolean variable on line 24 (Hint: true/false) on line 24
+        double dogsOut = true;  // the dogs are out :)
+
+        // define a character variable on line 27
+        char letter = 'A';
+
+        // define a string variable on line 30 
+        char name = "Jeff";
+        
+    }
+ }
+   
