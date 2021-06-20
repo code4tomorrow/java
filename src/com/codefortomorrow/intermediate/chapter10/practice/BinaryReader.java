173383a98e0aef3801232b3b2392402039624694
manasser99 <84741910+manasser99@users.noreply.github.com>
2021-06-20 18:07:20 -0400

    Add files via upload

---
+++ src/com/codefortomorrow/intermediate/chapter10/practice/BinaryReader.java
@@ -0,0 +1,28 @@
+/*
+    Create a program called BinaryReader in which a 4 x 4
+    array of booleans is created. Have the computer loop
+    through the array, printing a 0 if the current element is
+    set to false and a 1 if it set to true.
+
+    Example Array:
+    {
+        {true, false, false, true},
+        {false, true, true, true},
+        {true, true, false, true},
+        {true, false, false, false}
+    }
+
+    Example Output:
+    1001
+    0111
+    1101
+    1000
+*/
+
+public class BinaryReader
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
