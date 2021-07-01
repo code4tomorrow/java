89c8bc7d8f39149d3cf65f35541e490815016712
phrdang <phrdang@users.noreply.github.com>
2021-07-01 01:16:27 +0000

    Bot: Prettified Java code!

--- src/com/codefortomorrow/beginner/chapter1/practice/Comments.java
+++ src/com/codefortomorrow/beginner/chapter1/practice/Comments.java
@@ -3,16 +3,17 @@ package com.codefortomorrow.beginner.chapter1.practice;
 /**
  * @author ArmeetJatyani
  * March 2021
- * 
- * You'll be writing your first ever comment today! 
- * What we'll go over: 
+ *
+ * You'll be writing your first ever comment today!
+ * What we'll go over:
  * - single line comments
  * - multi line comments
  */
 
- // a class is an "object" where we will place all our code inside
- public class Comments {
-     // the main method (below) is the first thing that runs when your program is run
+// a class is an "object" where we will place all our code inside
+public class Comments {
+
+    // the main method (below) is the first thing that runs when your program is run
     public static void main(String[] args) {
         // this is a single line comment, I can write anything here
         // single line comments aren't run by Java!
@@ -22,17 +23,11 @@ package com.codefortomorrow.beginner.chapter1.practice;
          * this is a multi
          * line
          * comment
-         * 
+         *
          * It can span across multiple lines!
          */
 
         // YOUR ASSIGNMENT: write 1 single-line comment and 1 multi-line comment on the lines below...
 
-
-
-
-
-
-
     }
- }
+}
